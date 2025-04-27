0cf7ccb8c59ce0fd0f02bda09aa7be31fced2bf5
Samuel Lopes <samuellopes12390@gmail.com>
2025-04-27 11:28:49 -0300

    feat: Adição de Contexto Global, Navegação no Botão de Doctors e Alteração de Scroll

--- Doctor_Appointment_Booking_System/frontend/src/components/TopDoctors.jsx
+++ Doctor_Appointment_Booking_System/frontend/src/components/TopDoctors.jsx
@@ -1,8 +1,11 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { doctors } from "../assets/assets_frontend/assets";
+
+import { AppContext } from "../context/AppContext";
 
 const TopDoctors = () => {
   const navigate = useNavigate();
+  const { doctors } = useContext(AppContext);
 
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
@@ -29,7 +32,13 @@ const TopDoctors = () => {
           </div>
         ))}
       </div>
-      <button className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10 cursor-pointer">
+      <button
+        onClick={() => {
+          navigate("/doctors");
+          scrollTo(0, 0);
+        }}
+        className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10 cursor-pointer"
+      >
         more
       </button>
     </div>
