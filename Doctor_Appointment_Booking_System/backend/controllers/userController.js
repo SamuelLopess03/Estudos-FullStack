b78e89fd5388143f0005650ed46cd499a6f31325
Samuel Lopes <samuellopes12390@gmail.com>
2025-05-14 08:33:14 -0300

    feat: Criado a Rota e Controller de Registro do User na Aplicação Backend

---
+++ Doctor_Appointment_Booking_System/backend/controllers/userController.js
@@ -0,0 +1,60 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+import userModel from "../models/userModel.js";
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing Details",
+      });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({
+        success: false,
+        message: "Enter a Valid Email",
+      });
+    }
+
+    if (password.length < 8) {
+      return res.status(400).json({
+        success: false,
+        message: "Enter a Strong Password",
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const userData = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+
+    res.status(201).json({
+      success: true,
+      message: "User Registered Successfully",
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { registerUser };
