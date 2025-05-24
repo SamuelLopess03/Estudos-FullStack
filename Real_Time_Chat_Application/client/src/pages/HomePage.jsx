cece7591c6d7a27cf2ba564b077c9ae41901fdff
Samuel Lopes <samuellopes12390@gmail.com>
2025-05-24 09:04:35 -0300

    feat: Criação Inicial dos Components e Adição dos Mesmos em Home

--- Real_Time_Chat_Application/client/src/pages/HomePage.jsx
+++ Real_Time_Chat_Application/client/src/pages/HomePage.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
+import ChatContainer from "../components/ChatContainer";
+import Sidebar from "../components/Sidebar";
+import RightSidebar from "../components/RightSidebar";
+
 const HomePage = () => {
+  const [selectedUser, setSelectedUser] = useState(null);
+
   return (
-    <div>
-      <h1>HomePage</h1>
+    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+      <div
+        className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative 
+          ${
+            selectedUser
+              ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]"
+              : "md:grid-cols-2"
+          }`}
+      >
+        <Sidebar
+          setSelectedUser={setSelectedUser}
+          selectedUser={selectedUser}
+        />
+        <ChatContainer />
+        <RightSidebar />
+      </div>
     </div>
   );
 };
