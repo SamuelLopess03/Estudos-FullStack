a3110496e572124dfdf25ea8b3d73c804bfb5ab0
Samuel Lopes <samuellopes12390@gmail.com>
2025-05-26 11:21:23 -0300

    chore: Criação e Configuração Inicial do Backend com Mongoose

---
+++ Real_Time_Chat_Application/server/lib/db.js
@@ -0,0 +1,14 @@
+import e from "express";
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Database Connected");
+    });
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`);
+  } catch (error) {
+    console.error(error);
+  }
+};
---
+++ Real_Time_Chat_Application/server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import http from "http";
+import "dotenv/config";
+
+import { connectDB } from "./lib/db.js";
+
+// Create Express App and HTTP Server
+const app = express();
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 5000;
+
+// Middleware Setup
+app.use(express.json({ limit: "4mb" }));
+app.use(cors());
+
+app.use("/api/status", (req, res) => res.send("Server is Live"));
+
+// Connect to MongoDB
+await connectDB();
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
