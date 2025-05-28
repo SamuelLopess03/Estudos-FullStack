a5e586d23806eb0c5e7215b3dd1a24e1bf0b13f2
Samuel Lopes <samuellopes12390@gmail.com>
2025-05-28 09:37:31 -0300

    feat(server): Adicionando um Servidor WebSocket à Aplicação Backend

--- Real_Time_Chat_Application/server/server.js
+++ Real_Time_Chat_Application/server/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
-import http from "http";
 import "dotenv/config";
+
+import http from "http";
+import { Server } from "socket.io";
 
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/user.js";
@@ -10,6 +12,32 @@ import messageRouter from "./routes/message.js";
 // Create Express App and HTTP Server
 const app = express();
 const server = http.createServer(app);
+
+// Initialize Socket.IO Server
+export const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+// Store Online Users
+export const userSocketMap = {};
+
+// Socket.IO Connection Handler
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("User Connected:", userId);
+
+  if (userId) userSocketMap[userId] = socket.id;
+
+  // Emit Online Users to all Connected Clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected:", userId);
+    delete userSocketMap[userId];
+
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 
