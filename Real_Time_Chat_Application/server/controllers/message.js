5272be68951ba9ac2daa2fb8566383a6012caed7
Samuel Lopes <samuellopes12390@gmail.com>
2025-05-28 08:02:38 -0300

    feat(message-controller): Criado as Funções de Controller no Backend

---
+++ Real_Time_Chat_Application/server/controllers/message.js
@@ -0,0 +1,92 @@
+import User from "../models/user.js";
+import Message from "../models/message.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const filteredUsers = await User.find({ _id: { $ne: userId } }).select(
+      "-password"
+    );
+
+    const unseenMessages = {};
+
+    const promises = filteredUsers.map(async (user) => {
+      const messages = await Message.find({
+        senderId: user._id,
+        receiverId: userId,
+        seen: false,
+      });
+
+      if (messages.length > 0) {
+        unseenMessages[user._id] = messages.length;
+      }
+    });
+
+    await Promise.all(promises);
+
+    res.status(200).json({
+      success: true,
+      users: filteredUsers,
+      unseenMessages,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: selectedUserId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: selectedUserId },
+        { senderId: selectedUserId, receiverId: myId },
+      ],
+    });
+
+    await Message.updateMany(
+      {
+        senderId: selectedUserId,
+        receiverId: myId,
+      },
+      { seen: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      messages,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const markMessageAsSeen = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await Message.findByIdAndUpdate(id, { seen: true });
+
+    res.status(200).json({
+      success: true,
+      message: "Message marked as seen",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
