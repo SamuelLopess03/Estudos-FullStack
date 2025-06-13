b25964ae3ef83ee1a89ed4509c689545fe18328c
Samuel Lopes <samuellopes12390@gmail.com>
2025-06-13 09:41:19 -0300

    feat(MyBookings-Page): Criação da Página e Roteamento da Mesma em App

--- Hotel_Booking_System/client/src/App.jsx
+++ Hotel_Booking_System/client/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import AllRooms from "./pages/AllRooms";
 import RoomsDetails from "./pages/RoomDetails";
+import MyBookings from "./pages/MyBookings";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -19,6 +20,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomsDetails />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
         </Routes>
       </div>
 
