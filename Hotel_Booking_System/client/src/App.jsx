0d7b71e824177d78c1b3ac2ab24e697018b328b7
Samuel Lopes <samuellopes12390@gmail.com>
2025-06-13 07:59:39 -0300

    feat(RoomDetails-Page): Finalização da Página e Roteamento da Mesma em App

--- Hotel_Booking_System/client/src/App.jsx
+++ Hotel_Booking_System/client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes, useLocation } from "react-router-dom";
 
 import Home from "./pages/Home";
 import AllRooms from "./pages/AllRooms";
+import RoomsDetails from "./pages/RoomDetails";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
+          <Route path="/rooms/:id" element={<RoomsDetails />} />
         </Routes>
       </div>
 
