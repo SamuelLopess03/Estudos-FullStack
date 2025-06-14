2b79c97a94d61072420254514f616bbdd92d39f5
Samuel Lopes <samuellopes12390@gmail.com>
2025-06-14 08:00:01 -0300

    feat(SidebarOwner-Component): Finalização do Componente e Adição do Mesmo em Layout

--- Hotel_Booking_System/client/src/App.jsx
+++ Hotel_Booking_System/client/src/App.jsx
@@ -6,6 +6,10 @@ import RoomsDetails from "./pages/RoomDetails";
 import MyBookings from "./pages/MyBookings";
 import Layout from "./pages/hotelOwner/Layout";
 
+import Dashboard from "./pages/hotelOwner/Dashboard";
+import AddRoom from "./pages/hotelOwner/AddRoom";
+import ListRoom from "./pages/hotelOwner/ListRoom";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import HotelReg from "./components/HotelReg";
@@ -25,7 +29,11 @@ function App() {
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomsDetails />} />
           <Route path="/my-bookings" element={<MyBookings />} />
-          <Route path="/owner" element={<Layout />} />
+          <Route path="/owner" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="add-room" element={<AddRoom />} />
+            <Route path="list-room" element={<ListRoom />} />
+          </Route>
         </Routes>
       </div>
 
