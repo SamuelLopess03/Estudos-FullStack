d4b828bda7f12b1ebbdd8223a62fba1e9c3e81f5
Samuel Lopes <samuellopes12390@gmail.com>
2025-06-17 10:16:22 -0300

    [Hotel_Booking_System] chore: Adicionando a Dependência do Clerk no Backend
    O Clerk é basicamente um serviço de autenticação e autorização.

--- Hotel_Booking_System/server/server.js
+++ Hotel_Booking_System/server/server.js
@@ -2,12 +2,16 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 
+import { clerkMiddleware } from "@clerk/express";
+
 import connectDB from "./configs/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
 
 connectDB();
 
