fc1fe962ade4595d22181c07d9b747ea36de4115
Samuel Lopes <samuellopes12390@gmail.com>
2025-06-18 11:13:05 -0300

    [Authify_system] feat(UserRepository): Adição da Interface que extends de JpaRepository

---
+++ Authify_System/backend/src/main/java/com/samuel/authify/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.samuel.authify.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.samuel.authify.entities.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+	Optional<UserEntity> findByEmail(String email);
+}
