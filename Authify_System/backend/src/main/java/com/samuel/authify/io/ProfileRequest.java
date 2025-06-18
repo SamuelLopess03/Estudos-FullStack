d52609a7933bf1c8c8ec59916682e4c822a92e1f
Samuel Lopes <samuellopes12390@gmail.com>
2025-06-18 11:22:12 -0300

    [Authify_system] feat(io): Incluído Classes de Entrada e Saída de Dados da Aplicação

---
+++ Authify_System/backend/src/main/java/com/samuel/authify/io/ProfileRequest.java
@@ -0,0 +1,16 @@
+package com.samuel.authify.io;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ProfileRequest {
+	
+	private String name;
+	
+	private String email;
+	
+	private String password;
+	
+}
