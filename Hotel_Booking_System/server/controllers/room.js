f9bb5e22e9c306bfd54965763c59b20182f97275
Samuel Lopes <samuellopes12390@gmail.com>
2025-06-21 09:58:51 -0300

    [Hotel_Booking_System] feat(roomController): Feito o Controlador de Requisições para Rooms
    Inicialmente fizemos o mapeamento dos possíveis métodos de implementação
    do controlador.

---
+++ Hotel_Booking_System/server/controllers/room.js
@@ -0,0 +1,7 @@
+export const createRoom = async (req, res) => {};
+
+export const getRooms = async (req, res) => {};
+
+export const getOwnerRooms = async (req, res) => {};
+
+export const toggleRoomAvailability = async (req, res) => {};
