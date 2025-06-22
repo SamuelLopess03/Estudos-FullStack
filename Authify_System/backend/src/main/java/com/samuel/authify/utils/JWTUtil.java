9607421e00e6d41b9761aaf99e02b507864aeb06
Samuel Lopes <samuellopes12390@gmail.com>
2025-06-22 11:46:49 -0300

    [Authify_system] feat(JWTUtil): Adicionado Métodos de Validação e Resgate de Dados do Token

--- Authify_System/backend/src/main/java/com/samuel/authify/utils/JWTUtil.java
+++ Authify_System/backend/src/main/java/com/samuel/authify/utils/JWTUtil.java
@@ -3,11 +3,13 @@ package com.samuel.authify.utils;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -22,6 +24,30 @@ public class JWTUtil {
 		
 		return createToken(claims, userDetails.getUsername());
 	}
+	
+	public Boolean validateToken(String token, UserDetails userDetails) {
+		final String email = extractEmail(token);
+		
+		return (email.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
+	
+	public String extractEmail(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+	
+	public Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExpiration);
+	}
+	
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		
+		return claimsResolver.apply(claims);
+	}
+	
+	private Boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
 
 	private String createToken(Map<String, Object> claims, String email) {
 		return Jwts.builder()
@@ -32,4 +58,12 @@ public class JWTUtil {
 			.signWith(SignatureAlgorithm.HS256, SECRET_KEY)
 			.compact();
 	}
+	
+	private Claims extractAllClaims(String token) {
+		return Jwts.parser()
+				.setSigningKey(SECRET_KEY)
+				.parseClaimsJws(token)
+				.getBody();
+	}
+	
 }
