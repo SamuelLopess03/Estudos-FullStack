deacdb692249e309016d51aefc5e292692653cd7
Samuel Lopes <samuellopes12390@gmail.com>
2025-06-23 11:46:42 -0300

    [Authify_system] feat(profileController): Adicionado o Método que Chama o getProfile do ProfileService

--- Authify_System/backend/src/main/java/com/samuel/authify/controllers/ProfileController.java
+++ Authify_System/backend/src/main/java/com/samuel/authify/controllers/ProfileController.java
@@ -1,9 +1,10 @@
 package com.samuel.authify.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +29,11 @@ public class ProfileController {
 		// TODO: Send Welcome Email
 		
 		return response;
+	}
+	
+	@GetMapping("/profile")
+	public ProfileResponse getProfile(@CurrentSecurityContext(expression = "authentication?.name") String email) {
+		return profileService.getProfile(email);
 	}
 
 }
