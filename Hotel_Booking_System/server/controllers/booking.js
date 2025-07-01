08f2ac6c9804c3fd6cec01e3e929f9ad2ef3a939
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-01 10:06:35 -0300

    [Hotel_Booking_System] feat(bookingController): Estruturado os Dados de Envio de Email e Invocado a Função que Realiza o Envio

--- Hotel_Booking_System/server/controllers/booking.js
+++ Hotel_Booking_System/server/controllers/booking.js
@@ -1,3 +1,5 @@
+import transporter from "../configs/nodemailer.js";
+
 import booking from "../models/booking.js";
 import hotel from "../models/hotel.js";
 import room from "../models/room.js";
@@ -127,6 +129,31 @@ export const createBooking = async (req, res) => {
       checkOutDate,
       totalPrice,
     });
+
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: req.user.email,
+      subject: "Hotel Booking Details",
+      html: `
+        <h2>Booking Details</h2>
+        <p>Dear ${req.user.username},</p>
+        <p>Thank you for your booking! Here are your details:</p>
+        <ul>
+          <li><strong>Booking ID:</strong> ${bookingData._id}</li>
+          <li><strong>Hotel Name:</strong> ${roomData.hotel.name}</li>
+          <li><strong>Location:</strong> ${roomData.hotel.address}</li>
+          <li><strong>Date:</strong> ${bookingData.checkInDate.toDateString()}</li>
+          <li>
+            <strong>Booking Amount:</strong> 
+            ${process.env.CURRENCY || "$"} ${bookingData.totalPrice}/night
+          </li>
+        </ul>
+        <p>We look forward to welcoming you!</p>
+        <p>If you need to make any changes, feel free to contact us.</p>
+      `,
+    };
+
+    await transporter.sendMail(mailOptions);
 
     res.status(201).json({
       success: true,
