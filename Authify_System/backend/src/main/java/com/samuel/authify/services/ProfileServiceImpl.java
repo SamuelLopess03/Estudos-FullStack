6a3a43d9cc096816f5bb406f2b7e9ab887187d28
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-04 17:02:01 -0300

    [Authify_System] feat(profileService): Desenvolvido os Métodos de Geração e Verificação do Código OTP

--- Authify_System/backend/src/main/java/com/samuel/authify/services/ProfileServiceImpl.java
+++ Authify_System/backend/src/main/java/com/samuel/authify/services/ProfileServiceImpl.java
@@ -89,6 +89,54 @@ public class ProfileServiceImpl implements ProfileService {
 		
 		userRepository.save(existingUser);
 	}
+	
+	@Override
+	public void sendOtp(String email) {
+		UserEntity existingUser = userRepository.findByEmail(email)
+				.orElseThrow(() -> new UsernameNotFoundException("User not Found: " + email));
+		
+		if(existingUser.getIsAccountVerified() != null && 
+				existingUser.getIsAccountVerified()) {
+			return;
+		}
+		
+		String otp = String.valueOf(ThreadLocalRandom.current().nextInt(100000, 1000000));
+		
+		long expiryTime = System.currentTimeMillis() + (24 * 60 * 60 * 1000);
+		
+		existingUser.setVerifyOtp(otp);
+		existingUser.setVerifyOtpExpireAt(expiryTime);
+		
+		userRepository.save(existingUser);
+		
+		try {
+			emailService.sendOtpEmail(existingUser.getEmail(), otp);
+		} catch (Exception e) {
+			throw new RuntimeException("Unable to Send Email");
+		}
+		
+	}
+
+	@Override
+	public void verifyOtp(String email, String otp) {
+		UserEntity existingUser = userRepository.findByEmail(email)
+				.orElseThrow(() -> new UsernameNotFoundException("User not Found: " + email));
+		
+		if(existingUser.getVerifyOtp() == null ||
+				!existingUser.getVerifyOtp().equals(otp)) {
+			throw new RuntimeException("Invalid OTP");
+		}
+		
+		if(existingUser.getVerifyOtpExpireAt() < System.currentTimeMillis()) {
+			throw new RuntimeException("OTP Expired");
+		}
+		
+		existingUser.setIsAccountVerified(true);
+		existingUser.setVerifyOtp(null);
+		existingUser.setVerifyOtpExpireAt(0L);
+		
+		userRepository.save(existingUser);
+	}
 
 	private UserEntity convertToUserEntity(ProfileRequest request) {
 		
