cb8224b9fd1fd45858b84c897e3953b9a8ae0150
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-07 10:34:12 -0300

    [Authify_System] feat(mainJSX): Adicionando Dependências do Bootstrap na Aplicação Frontend

--- Authify_System/frontend/src/main.jsx
+++ Authify_System/frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
