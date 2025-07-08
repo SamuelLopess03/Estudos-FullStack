5fe25733e5f1a2ddc169da0deba212c16549e127
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-08 10:00:11 -0300

    [Spotify_Clone] feat(index): Adicionado a Conexão com o Banco de Dados e Roteamento das Rotas no Index

--- Spotify_Clone/user-service/src/index.ts
+++ Spotify_Clone/user-service/src/index.ts
@@ -1,12 +1,36 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+import userRoutes from "./route.js";
 
 dotenv.config();
+
+const connectDb = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Connected to MongoDB");
+    });
+
+    await mongoose.connect(process.env.MONGO_URI as string, {
+      dbName: "spotify",
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Server is Working!");
+});
+
+app.use("/api/v1", userRoutes);
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  connectDb();
 });
