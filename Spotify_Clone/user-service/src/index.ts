3b9dfcd70c4027dd45207e6aed91c74651dbc718
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-08 10:09:13 -0300

    [Spotify_Clone] fix: Modificado Validação do Campo Role de Usuário e Adicionado Express.JSON em Index

--- Spotify_Clone/user-service/src/index.ts
+++ Spotify_Clone/user-service/src/index.ts
@@ -24,6 +24,8 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Server is Working!");
 });
--- Spotify_Clone/user-service/src/model.ts
+++ Spotify_Clone/user-service/src/model.ts
@@ -13,7 +13,7 @@ const schema: Schema<IUser> = new Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, required: true },
+    role: { type: String, default: "user" },
     playlist: [{ type: String, required: true }],
   },
   { timestamps: true }
