8d3b737ffd3613bc23712952bfde91c4334fe1ce
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-09 09:12:21 -0300

    [Spotify_Clone] feat(db): Adicionado as Configurações de Conexão com o banco de dados PostgreSQL (Neon)
    Além disso, foi criado uma função que realiza a conexão com o banco
    de dados e inicializa as tabelas.

--- Spotify_Clone/admin-service/src/index.ts
+++ Spotify_Clone/admin-service/src/index.ts
@@ -1,12 +1,46 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import { sql } from "./configs/db.js";
+
 dotenv.config();
+
+async function initDB() {
+  try {
+    await sql`
+        CREATE TABLE IF NOT EXISTS albums(
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            description VARCHAR(255) NOT NULL,
+            thumbnail VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    `;
+
+    await sql`
+        CREATE TABLE IF NOT EXISTS songs(
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            description VARCHAR(255) NOT NULL,
+            thumbnail VARCHAR(255),
+            audio VARCHAR(255) NOT NULL,
+            album_id INTEGER REFERENCES albums(id) ON DELETE SET NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    `;
+
+    console.log("Database Initialized Successfully");
+  } catch (error) {
+    console.log("Error InitDB: ", error);
+  }
+}
 
 const app = express();
 
 const PORT = process.env.PORT || 7000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
