e4ad95aae3bf6e1d3ebe785eedf87de05b857370
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-10 07:40:19 -0300

    [Authify_System] feat(menubarComponente): Adicionado a Utilização do Contexto Global no Componente

--- Authify_System/frontend/src/components/Menubar.jsx
+++ Authify_System/frontend/src/components/Menubar.jsx
@@ -1,9 +1,48 @@
+import { useContext, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 import { assets } from "../assets/assets.js";
+import { AppContext } from "../context/AppContext.jsx";
 
 const Menubar = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const dropdownRef = useRef(null);
+
+  const { backendUrl, userData, setUserData, setIsLoggedIn } =
+    useContext(AppContext);
+
   const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+
+      const response = await axios.post(backendUrl + "/logout");
+
+      if (response.status === 200) {
+        setIsLoggedIn(false);
+        setUserData(false);
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
 
   return (
     <nav className="navbar bg-white px-5 py-4 d-flex justify-content-between align-items-center">
@@ -12,12 +51,58 @@ const Menubar = () => {
         <span className="fw-bold fs-4 text-dark">Authify</span>
       </div>
 
-      <div
-        className="btn btn-outline-dark rounded-pill px-3"
-        onClick={() => navigate("/login")}
-      >
-        Login <i className="bi bi-arrow-right ms-2"></i>
-      </div>
+      {userData ? (
+        <div className="position-relative" ref={dropdownRef}>
+          <div
+            className="bg-dark text-white rounded-circle d-flex justify-content-center align-items-center"
+            style={{
+              width: "40px",
+              height: "40px",
+              cursor: "pointer",
+              userSelect: "none",
+            }}
+            onClick={() => setDropdownOpen((prev) => !prev)}
+          >
+            {console.log(userData)}
+            {userData.name[0].toUpperCase()}
+          </div>
+
+          {dropdownOpen && (
+            <div
+              className="position-absolute shadow bg-white rounded p-2"
+              style={{
+                top: "50px",
+                right: "0",
+                zIndex: "100",
+              }}
+            >
+              {!userData.isAccountVerified && (
+                <div
+                  className="dropdown-item py-1 px-2"
+                  style={{ cursor: "pointer" }}
+                >
+                  Verify Email
+                </div>
+              )}
+
+              <div
+                className="dropdown-item py-1 px-2 text-danger"
+                style={{ cursor: "pointer" }}
+                onClick={handleLogout}
+              >
+                Logout
+              </div>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div
+          className="btn btn-outline-dark rounded-pill px-3"
+          onClick={() => navigate("/login")}
+        >
+          Login <i className="bi bi-arrow-right ms-2"></i>
+        </div>
+      )}
     </nav>
   );
 };
