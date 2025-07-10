a883005446dc666d92c2ebb5d4d75ff2f6ba21c9
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-10 11:04:04 -0300

    [Spotify_Clone] feat(controller): Desenvolvido e Roteado o Método que Adiciona uma Música em AdminService
    Tal Música (Song) deve está associada a um Album já criado anteriormente

--- Spotify_Clone/admin-service/src/controller.ts
+++ Spotify_Clone/admin-service/src/controller.ts
@@ -52,3 +52,55 @@ export const addAlbum = tryCatch(async (req: AuthenticatedRequest, res) => {
     album: result[0],
   });
 });
+
+export const addSong = tryCatch(async (req: AuthenticatedRequest, res) => {
+  if (req.user?.role !== "admin") {
+    res.status(403).json({
+      message: "You are not Admin",
+    });
+
+    return;
+  }
+
+  const { title, description, album } = req.body;
+
+  const isAlbum = await sql`SELECT * FROM albums WHERE id = ${album}`;
+
+  if (isAlbum.length === 0) {
+    res.status(404).json({
+      message: "No Album With This Id",
+    });
+
+    return;
+  }
+
+  const file = req.file;
+
+  if (!file) {
+    res.status(400).json({
+      message: "No File to Upload",
+    });
+
+    return;
+  }
+
+  const fileBuffer = getBuffer(file);
+
+  if (!fileBuffer || !fileBuffer.content) {
+    throw new Error("Failed to Generate File Buffer");
+  }
+
+  const cloud = await cloudinary.v2.uploader.upload(fileBuffer.content, {
+    folder: "songs",
+    resource_type: "video",
+  });
+
+  const result = await sql`
+    INSERT INTO songs (title, description, audio, album_id) VALUES 
+    (${title}, ${description}, ${cloud.secure_url}, ${album})
+  `;
+
+  res.status(201).json({
+    message: "Song Added Successfully",
+  });
+});
--- Spotify_Clone/admin-service/src/route.ts
+++ Spotify_Clone/admin-service/src/route.ts
@@ -1,10 +1,11 @@
 import express from "express";
 
 import { isAuth, uploadFile } from "./middleware.js";
-import { addAlbum } from "./controller.js";
+import { addAlbum, addSong } from "./controller.js";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/album/new", isAuth, uploadFile, addAlbum);
+adminRouter.post("/song/new", isAuth, uploadFile, addSong);
 
 export default adminRouter;
