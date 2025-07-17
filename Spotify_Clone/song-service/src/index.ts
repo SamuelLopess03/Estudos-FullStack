61dc661542720d965691e66a09e432c3845d6a5e
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-17 11:10:22 -0300

    [Spotify_Clone] feat(songService): Criado o Roteador da Aplicação e Adicionado no Index

--- Spotify_Clone/song-service/src/index.ts
+++ Spotify_Clone/song-service/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import songRouter from "./route.js";
 
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT || 8000;
+
+app.use("/api/v1", songRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
