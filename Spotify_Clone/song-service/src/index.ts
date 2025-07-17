a430bd1530ca6a929c1c90dab8811d3de930e68f
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-17 11:40:27 -0300

    [Spotify_Clone] feat(songService): Adicionado uma Função que Realiza a Conexão com o Banco de Dados (Neon) no Index

--- Spotify_Clone/song-service/src/index.ts
+++ Spotify_Clone/song-service/src/index.ts
@@ -1,8 +1,41 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import songRouter from "./route.js";
 
+import { sql } from "./configs/db.js";
+
 dotenv.config();
+
+async function initDB() {
+  try {
+    await sql`
+        CREATE TABLE IF NOT EXISTS albums(
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            description VARCHAR(255) NOT NULL,
+            thumbnail VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    `;
+
+    await sql`
+        CREATE TABLE IF NOT EXISTS songs(
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            description VARCHAR(255) NOT NULL,
+            thumbnail VARCHAR(255),
+            audio VARCHAR(255) NOT NULL,
+            album_id INTEGER REFERENCES albums(id) ON DELETE SET NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    `;
+
+    console.log("Database Initialized Successfully");
+  } catch (error) {
+    console.log("Error InitDB: ", error);
+  }
+}
 
 const app = express();
 
@@ -10,6 +43,8 @@ const port = process.env.PORT || 8000;
 
 app.use("/api/v1", songRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+initDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 });
