6f5924454de05d44d75e48f0ecf27e08e88752a5
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-18 09:58:32 -0300

    [Spotify_Clone] refactor(songService): Removendo Função de Criação de Tabelas no Banco de Dados Desnecessária do Index
    Antes eu pensava que tal função era necessária para conectar
    ao banco de dados, mas observando e pensando melhor ela não
    fazia sentido, com isso decidi removê-la.

--- Spotify_Clone/song-service/src/index.ts
+++ Spotify_Clone/song-service/src/index.ts
@@ -1,41 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import redis from "redis";
 
 import songRouter from "./route.js";
 
-import { sql } from "./configs/db.js";
-
 dotenv.config();
-
-async function initDB() {
-  try {
-    await sql`
-        CREATE TABLE IF NOT EXISTS albums(
-            id SERIAL PRIMARY KEY,
-            title VARCHAR(255) NOT NULL,
-            description VARCHAR(255) NOT NULL,
-            thumbnail VARCHAR(255) NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
-    `;
-
-    await sql`
-        CREATE TABLE IF NOT EXISTS songs(
-            id SERIAL PRIMARY KEY,
-            title VARCHAR(255) NOT NULL,
-            description VARCHAR(255) NOT NULL,
-            thumbnail VARCHAR(255),
-            audio VARCHAR(255) NOT NULL,
-            album_id INTEGER REFERENCES albums(id) ON DELETE SET NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
-    `;
-
-    console.log("Database Initialized Successfully");
-  } catch (error) {
-    console.log("Error InitDB: ", error);
-  }
-}
 
 const app = express();
 
@@ -43,8 +12,6 @@ const port = process.env.PORT || 8000;
 
 app.use("/api/v1", songRouter);
 
-initDB().then(() => {
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
