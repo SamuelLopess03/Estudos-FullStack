39449886ed19a5a5b3c3c8ab257d63d1db71e923
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-18 11:34:35 -0300

    [Spotify_Clone] feat(songService): Adicionado Configuração e Conexão com o Redis no Index

--- Spotify_Clone/song-service/src/index.ts
+++ Spotify_Clone/song-service/src/index.ts
@@ -6,6 +6,21 @@ import songRouter from "./route.js";
 
 dotenv.config();
 
+export const redisClient = redis.createClient({
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: "redis-16936.c308.sa-east-1-1.ec2.redns.redis-cloud.com",
+    port: 16936,
+  },
+});
+
+redisClient
+  .connect()
+  .then(() => {
+    console.log("Connected to Redis");
+  })
+  .catch(console.error);
+
 const app = express();
 
 const port = process.env.PORT || 8000;
