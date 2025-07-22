8508c878e8ee0d852a20dc02d51f3d0662028d12
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-22 17:01:36 -0300

    [Spotify_Clone] feat(frontend): Mapeado a Listagem de Albums com Componente AlbumCard na Página Home

--- Spotify_Clone/frontend/src/pages/Home.tsx
+++ Spotify_Clone/frontend/src/pages/Home.tsx
@@ -1,9 +1,29 @@
+import { useSongData } from "../context/SongContext";
+
+import AlbumCard from "../components/AlbumCard";
 import Layout from "../components/Layout";
 
 const Home = () => {
+  const { albums, songs } = useSongData();
+
   return (
     <div>
-      <Layout>Home</Layout>
+      <Layout>
+        <div className="mb-4">
+          <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+          <div className="flex overflow-auto">
+            {albums?.map((e, i) => (
+              <AlbumCard
+                key={i}
+                id={e.id}
+                name={e.title}
+                desc={e.description}
+                image={e.thumbnail}
+              />
+            ))}
+          </div>
+        </div>
+      </Layout>
     </div>
   );
 };
