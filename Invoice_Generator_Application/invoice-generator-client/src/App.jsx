b49ffcb3b1284e794520cf4221f3038b99ce0434
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-23 10:06:35 -0300

    [Invoice_Generator] feat(app): Adicionado o Roteamento das Páginas no App do Frontend

---
+++ Invoice_Generator_Application/invoice-generator-client/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import Menubar from "./components/Menubar";
+
+import LandingPage from "./pages/LandingPage/LandingPage";
+import Dashboard from "./pages/Dashboard";
+import MainPage from "./pages/MainPage";
+import PreviewPage from "./pages/PreviewPage";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Menubar />
+
+      <Toaster />
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/generate" element={<MainPage />} />
+        <Route path="/preview" element={<PreviewPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
