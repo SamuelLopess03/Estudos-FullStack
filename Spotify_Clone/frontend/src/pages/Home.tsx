ae92f7821144f0bbc378cfca29626fe878e880c5
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-24 15:18:05 -0300

    [Spotify_Clone] feat(frontend): Implementado Validação de Loading na Página de Home
    Basicamente, essa validação vai verificar se os dados de albums
    e songs já foram resgatados da API, caso ainda não tenham sido
    encontrados é exibido o componente de Loading, mas se já estiverem
    prontos é exibido o componente normal de Layout.

--- Spotify_Clone/frontend/src/pages/Home.tsx
+++ Spotify_Clone/frontend/src/pages/Home.tsx
@@ -2,28 +2,49 @@ import { useSongData } from "../context/SongContext";
 
 import AlbumCard from "../components/AlbumCard";
 import Layout from "../components/Layout";
+import SongCard from "../components/SongCard";
+import Loading from "../components/Loading";
 
 const Home = () => {
-  const { albums, songs } = useSongData();
+  const { albums, songs, loading } = useSongData();
 
   return (
     <div>
-      <Layout>
-        <div className="mb-4">
-          <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
-          <div className="flex overflow-auto">
-            {albums?.map((e, i) => (
-              <AlbumCard
-                key={i}
-                id={e.id}
-                name={e.title}
-                desc={e.description}
-                image={e.thumbnail}
-              />
-            ))}
+      {loading ? (
+        <Loading />
+      ) : (
+        <Layout>
+          <div className="mb-4">
+            <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+            <div className="flex overflow-auto">
+              {albums?.map((e, i) => (
+                <AlbumCard
+                  key={i}
+                  id={e.id}
+                  name={e.title}
+                  desc={e.description}
+                  image={e.thumbnail}
+                />
+              ))}
+            </div>
           </div>
-        </div>
-      </Layout>
+
+          <div className="mb-4">
+            <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+            <div className="flex overflow-auto">
+              {songs?.map((e, i) => (
+                <SongCard
+                  key={i}
+                  id={e.id}
+                  name={e.title}
+                  desc={e.description}
+                  image={e.thumbnail}
+                />
+              ))}
+            </div>
+          </div>
+        </Layout>
+      )}
     </div>
   );
 };
