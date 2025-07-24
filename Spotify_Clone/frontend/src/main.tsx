8cf532555ca90cda8b9db187af16007e4de0842a
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-24 15:14:49 -0300

    [Spotify_Clone] feat(frontend): Adicionado o Provedor do Contexto Global de User no Arquivo Main

--- Spotify_Clone/frontend/src/main.tsx
+++ Spotify_Clone/frontend/src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import { SongProvider } from "./context/SongContext.tsx";
+import { UserProvider } from "./context/UserContext.tsx";
 
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <SongProvider>
-      <App />
-    </SongProvider>
+    <UserProvider>
+      <SongProvider>
+        <App />
+      </SongProvider>
+    </UserProvider>
   </StrictMode>
 );
