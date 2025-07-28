db33d267b3803396ee5dbb25b3de1bc8f1c631ae
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-28 16:53:12 -0300

    [Spotify_Clone] feat(frontend): Implementado o Roteamento das Páginas de PlayList e Admin da Aplicação

--- Spotify_Clone/frontend/src/App.tsx
+++ Spotify_Clone/frontend/src/App.tsx
@@ -7,6 +7,8 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Album from "./pages/Album";
+import PlayList from "./pages/PlayList";
+import Admin from "./pages/Admin";
 
 function App() {
   const { isAuth, loading } = useUserData();
@@ -19,12 +21,20 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/album/:id" element={<Album />} />
             <Route path="/login" element={isAuth ? <Home /> : <Login />} />
             <Route
               path="/register"
               element={isAuth ? <Home /> : <Register />}
             />
-            <Route path="/album/:id" element={<Album />} />
+            <Route
+              path="/playlist"
+              element={isAuth ? <PlayList /> : <Login />}
+            />
+            <Route
+              path="/admin/dashboard"
+              element={isAuth ? <Admin /> : <Login />}
+            />
           </Routes>
         </BrowserRouter>
       )}
