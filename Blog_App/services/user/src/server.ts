35dc0cd8b281c3ed9f436e495efd8b838eda3755
Samuel Lopes <samuellopes12390@gmail.com>
2025-07-31 16:47:55 -0300

    [Blog_App] feat(service-user): Adicionado Roteador, Configurações do Cloudinary e Conexão do MongoDB ao Index

--- Blog_App/services/user/src/server.ts
+++ Blog_App/services/user/src/server.ts
@@ -1,11 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
+
+import connectDb from "./utils/db.js";
+
+import userRoutes from "./routes/user.js";
 
 dotenv.config();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
 const app = express();
 
+connectDb();
+
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api/v1", userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
