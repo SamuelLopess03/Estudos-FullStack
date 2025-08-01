409a9f3d5ad8d3927ed7a9725646242192f5b230
Samuel Lopes <samuellopes12390@gmail.com>
2025-08-01 16:15:24 -0300

    [Blog_App] feat(service-author): Desenvolvido o Middleware que Realiza a Autenticação do Usuário com Token JWT

---
+++ Blog_App/services/author/src/middlewares/isAuth.ts
@@ -0,0 +1,60 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+interface IUser extends Document {
+  _id: string;
+  name: string;
+  email: string;
+  image: string;
+  instagram: string;
+  facebook: string;
+  linkedin: string;
+  bio: string;
+}
+
+export interface AuthenticatedRequest extends Request {
+  user?: IUser | null;
+}
+
+export const isAuth = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      res.status(401).json({
+        message: "Please Login - No Auth Header",
+      });
+
+      return;
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decodeToken = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as JwtPayload;
+
+    if (!decodeToken || !decodeToken.user) {
+      res.status(401).json({
+        message: "Invalid Token",
+      });
+
+      return;
+    }
+
+    req.user = decodeToken.user;
+
+    next();
+  } catch (error) {
+    console.error("JWT Authentication Error: ", error);
+
+    res.status(401).json({
+      message: "Please Login - Jwt Error",
+    });
+  }
+};
