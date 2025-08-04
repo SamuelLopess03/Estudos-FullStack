5c439125d200303e86f4255ed5b1b42ec4a15b8e
Samuel Lopes <samuellopes12390@gmail.com>
2025-08-04 16:29:29 -0300

    [Blog_App] feat(service-blog): Adicionado a Configuração do Redis em Server

--- Blog_App/services/blog/src/server.ts
+++ Blog_App/services/blog/src/server.ts
@@ -1,11 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import { createClient } from "redis";
+
+import blogRoutes from "./routes/blog.js";
 
 dotenv.config();
+
+export const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+
+redisClient
+  .connect()
+  .then(() => console.log("Connected to Redis"))
+  .catch(console.error);
 
 const app = express();
 
 const port = process.env.PORT || 5002;
+
+app.use("/api/v1", blogRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
