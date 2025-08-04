f0bd074ccd9770579c3e44f04cb87113304d7d7e
Samuel Lopes <samuellopes12390@gmail.com>
2025-08-04 16:18:40 -0300

    [Blog_App] feat(services-user): Adicionado CORS à API do Microsserviço User

--- Blog_App/services/user/src/server.ts
+++ Blog_App/services/user/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { v2 as cloudinary } from "cloudinary";
 
 import connectDb from "./utils/db.js";
@@ -21,6 +22,7 @@ connectDb();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1", userRoutes);
 
