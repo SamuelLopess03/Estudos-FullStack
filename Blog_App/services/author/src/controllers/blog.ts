34e63262cd9dd85c6067d16ac9b8566ae2a2ea25
Samuel Lopes <samuellopes12390@gmail.com>
2025-08-04 16:20:09 -0300

    [Blog_App] feat(services-author): Implementado e Roteado os Métodos de Update e Delete de Blog

--- Blog_App/services/author/src/controllers/blog.ts
+++ Blog_App/services/author/src/controllers/blog.ts
@@ -41,3 +41,91 @@ export const createBlog = tryCatch(
     });
   }
 );
+
+export const updateBlog = tryCatch(
+  async (req: AuthenticatedRequest, res: Response) => {
+    const { id } = req.params;
+    const { title, description, blogcontent, category } = req.body;
+
+    const file = req.file;
+
+    const blog = await sql`SELECT * FROM blogs WHERE id = ${id}`;
+
+    if (!blog.length) {
+      res.status(404).json({
+        message: "No Blog With This ID",
+      });
+
+      return;
+    }
+
+    if (blog[0].author !== req.user?._id) {
+      res.status(403).json({
+        message: "You Are Not Author of This Blog",
+      });
+
+      return;
+    }
+
+    let imageUrl = blog[0].image;
+
+    if (file) {
+      const fileBuffer = getBuffer(file);
+
+      if (!fileBuffer || !fileBuffer.content) {
+        throw new Error("Failed to Generate Buffer.");
+      }
+
+      const cloud = await cloudinary.v2.uploader.upload(fileBuffer.content, {
+        folder: "blogs",
+      });
+
+      imageUrl = cloud.secure_url;
+    }
+
+    const updatedBlog = await sql`
+      UPDATE blogs SET title = ${title || blog[0].title}, description = ${
+      description || blog[0].description
+    },
+      image = ${imageUrl}, blogcontent = ${
+      blogcontent || blog[0].blogcontent
+    }, category = ${category || blog[0].category}
+      WHERE id = ${id} RETURNING *
+    `;
+
+    res.status(200).json({
+      message: "Blog Updated Successfully",
+      blog: updatedBlog[0],
+    });
+  }
+);
+
+export const deleteBlog = tryCatch(
+  async (req: AuthenticatedRequest, res: Response) => {
+    const blog = await sql`SELECT * FROM blogs WHERE id = ${req.params.id}`;
+
+    if (!blog.length) {
+      res.status(404).json({
+        message: "No Blog With This ID",
+      });
+
+      return;
+    }
+
+    if (blog[0].author !== req.user?._id) {
+      res.status(403).json({
+        message: "You Are Not Author of This Blog",
+      });
+
+      return;
+    }
+
+    await sql`DELETE FROM savedblogs WHERE blogid = ${req.params.id}`;
+    await sql`DELETE FROM comments WHERE blogid = ${req.params.id}`;
+    await sql`DELETE FROM blogs WHERE id = ${req.params.id}`;
+
+    res.status(200).json({
+      message: "Blog Deleted Successfully",
+    });
+  }
+);
