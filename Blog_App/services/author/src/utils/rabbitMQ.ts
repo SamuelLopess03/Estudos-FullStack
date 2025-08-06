96dc9302dd3e945daf22995a3039779e09501758
Samuel Lopes <samuellopes12390@gmail.com>
2025-08-06 13:49:36 -0300

    [Blog_App] feat(service-author): Desenvolvido o Arquivo que Contém os Métodos Utilitários do RabbitMQ

---
+++ Blog_App/services/author/src/utils/rabbitMQ.ts
@@ -0,0 +1,50 @@
+import amqp from "amqplib";
+
+let channel: amqp.Channel;
+
+export const connectRabbitMQ = async () => {
+  try {
+    const connection = await amqp.connect({
+      protocol: "amqp",
+      hostname: "localhost",
+      port: 5672,
+      username: "admin",
+      password: "admin123",
+    });
+
+    channel = await connection.createChannel();
+
+    console.log("Connected to RabbitMQ");
+  } catch (error) {
+    console.error("Failed to Connect to RabbitMQ", error);
+  }
+};
+
+export const publishToQueue = async (queueName: string, message: any) => {
+  if (!channel) {
+    console.error("RabbitMQ Channel is not Initialized");
+
+    return;
+  }
+
+  await channel.assertQueue(queueName, { durable: true });
+
+  channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), {
+    persistent: true,
+  });
+};
+
+export const invalidateCacheJob = async (cacheKeys: string[]) => {
+  try {
+    const message = {
+      action: "invalidateCache",
+      keys: cacheKeys,
+    };
+
+    await publishToQueue("cache-invalidation", message);
+
+    console.log("Cache Invalidation Job Published to RabbitMQ");
+  } catch (error) {
+    console.error("Failed to Publish Cache on RabbitMQ", error);
+  }
+};
