a060dd71b2e6ddd0752b99fcd18245ef3c469871
Samuel Lopes <samuellopes12390@gmail.com>
2025-08-09 17:56:39 -0300

    [Blog_App] feat(frontend): Desenvolvido o Contexto Geral da Aplicação e Adicionado o Provedor em Layout

---
+++ Blog_App/frontend/src/context/AppContext.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { Toaster } from "react-hot-toast";
+import Cookies from "js-cookie";
+import axios from "axios";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+export const user_service = "http://localhost:5000";
+export const author_service = "http://localhost:5001";
+export const blog_service = "http://localhost:5002";
+
+export interface User {
+  _id: string;
+  name: string;
+  email: string;
+  image: string;
+  instagram: string;
+  facebook: string;
+  linkedin: string;
+  bio: string;
+}
+
+export interface Blog {
+  id: string;
+  title: string;
+  description: string;
+  blogcontent: string;
+  image: string;
+  category: string;
+  author: string;
+  created_at: string;
+}
+
+interface AppContextType {
+  user: User | null;
+  loading: boolean;
+  isAuth: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsAuth: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AppContext = createContext<AppContextType | undefined>(undefined);
+
+interface AppProviderProps {
+  children: ReactNode;
+}
+
+export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isAuth, setIsAuth] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  async function fetchUser() {
+    try {
+      const token = Cookies.get("token");
+
+      const { data } = await axios.get<User>(`${user_service}/api/v1/me`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setUser(data);
+      setIsAuth(true);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  const value = {
+    user,
+    isAuth,
+    loading,
+    setIsAuth,
+    setLoading,
+  };
+
+  return (
+    <AppContext.Provider value={value}>
+      <GoogleOAuthProvider clientId="609014385615-0v8cgcm5aa1hbdb7gfqm2vtkj1c76nur.apps.googleusercontent.com">
+        {children}
+        <Toaster />
+      </GoogleOAuthProvider>
+    </AppContext.Provider>
+  );
+};
+
+export const useAppData = (): AppContextType => {
+  const context = useContext(AppContext);
+
+  if (!context) {
+    throw new Error("useAppData Must be Used Within AppProvider");
+  }
+
+  return context;
+};
