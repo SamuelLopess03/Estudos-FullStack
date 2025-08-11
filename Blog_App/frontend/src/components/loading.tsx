3f50aa66075dacbf4ddf031ac1058caa10edf398
Samuel Lopes <samuellopes12390@gmail.com>
2025-08-11 16:00:32 -0300

    [Blog_App] feat(frontend): Desenvolvido o Componente Loading da Aplicação

---
+++ Blog_App/frontend/src/components/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="w-[200px] m-auto mt-[300px]">
+      <p className="text-2xl text-blue-500 font-bold text-center">Loading...</p>
+    </div>
+  );
+};
+
+export default Loading;
