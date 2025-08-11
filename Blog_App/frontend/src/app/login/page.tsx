d896810d862b7a9bfd8b8516ca9c15e7681d9c89
Samuel Lopes <samuellopes12390@gmail.com>
2025-08-11 16:13:53 -0300

    [Blog_App] feat(frontend): Utilizado o Contexto Global da Aplicação na Página de Login

--- Blog_App/frontend/src/app/login/page.tsx
+++ Blog_App/frontend/src/app/login/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { redirect } from "next/navigation";
+
 import React from "react";
 import toast from "react-hot-toast";
 import Cookies from "js-cookie";
@@ -14,19 +16,26 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Loading from "@/components/loading";
 
-import { user_service } from "@/context/AppContext";
+import { useAppData, user_service, User } from "@/context/AppContext";
 
 const LoginPage = () => {
+  const { isAuth, loading, setIsAuth, setLoading, setUser } = useAppData();
+
+  if (isAuth) return redirect("/");
+
   const responseGoogle = async (authResult: any) => {
+    setLoading(true);
     try {
-      const result = await axios.post<{ token: string; message: string }>(
-        `${user_service}/api/v1/login`,
-        {
-          code: authResult["code"],
-          redirect_uri: "http://localhost:3000",
-        }
-      );
+      const result = await axios.post<{
+        token: string;
+        message: string;
+        user: User;
+      }>(`${user_service}/api/v1/login`, {
+        code: authResult["code"],
+        redirect_uri: "http://localhost:3000",
+      });
 
       Cookies.set("token", result.data.token, {
         expires: 5,
@@ -35,9 +44,13 @@ const LoginPage = () => {
       });
 
       toast.success(result.data.message);
+      setIsAuth(true);
+      setLoading(false);
+      setUser(result.data.user);
     } catch (error) {
       console.error("Error: ", error);
       toast.error("Problem While Login You");
+      setLoading(false);
     }
   };
 
@@ -48,20 +61,26 @@ const LoginPage = () => {
   });
 
   return (
-    <div className="w-[350px] m-auto mt-[200px]">
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle>Login to The Reading Retreat</CardTitle>
-          <CardDescription>Your go to Blog App</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Button onClick={googleLogin}>
-            Login With Google{" "}
-            <img src="/google.png" className="w-5 h-5" alt="Google Icon" />{" "}
-          </Button>
-        </CardContent>
-      </Card>
-    </div>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="w-[350px] m-auto mt-[200px]">
+          <Card className="w-[350px]">
+            <CardHeader>
+              <CardTitle>Login to The Reading Retreat</CardTitle>
+              <CardDescription>Your go to Blog App</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Button onClick={googleLogin}>
+                Login With Google{" "}
+                <img src="/google.png" className="w-5 h-5" alt="Google Icon" />{" "}
+              </Button>
+            </CardContent>
+          </Card>
+        </div>
+      )}
+    </>
   );
 };
 
