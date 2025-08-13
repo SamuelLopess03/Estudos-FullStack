2f0e62b9fd2cd70c2e4828bf1d828693ee5222ec
Samuel Lopes <samuellopes12390@gmail.com>
2025-08-13 15:56:11 -0300

    [Blog_App] feat(service-author): Realizado o Roteamento dos Novos Métodos que Conectam com o Google Gemini AI

--- Blog_App/services/author/src/routes/blog.ts
+++ Blog_App/services/author/src/routes/blog.ts
@@ -2,12 +2,23 @@ import express from "express";
 
 import { isAuth } from "../middlewares/isAuth.js";
 import uploadFile from "../middlewares/multer.js";
-import { createBlog, deleteBlog, updateBlog } from "../controllers/blog.js";
+import {
+  aiBlogResponse,
+  aiDescriptionResponse,
+  aiTitleResponse,
+  createBlog,
+  deleteBlog,
+  updateBlog,
+} from "../controllers/blog.js";
 
 const router = express.Router();
 
 router.post("/blog/new", isAuth, uploadFile, createBlog);
 router.put("/blog/:id", isAuth, uploadFile, updateBlog);
 router.delete("/blog/:id", isAuth, uploadFile, deleteBlog);
+
+router.post("/ai/title", aiTitleResponse);
+router.post("/ai/description", aiDescriptionResponse);
+router.post("/ai/blog", aiBlogResponse);
 
 export default router;
