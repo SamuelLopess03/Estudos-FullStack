d3ca2229d3f0d5695bf9820f077fe1932338d827
Samuel Lopes <samuellopes12390@gmail.com>
2025-08-14 15:02:48 -0300

    [Blog_App] feat(frontend): Desenvolvido o Componente Personalizado do Sidebar para Aplicação

---
+++ Blog_App/frontend/src/components/sidebar.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import React from "react";
+import { BoxSelect } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "./ui/sidebar";
+import { Input } from "./ui/input";
+
+import { blogCategories } from "@/app/blog/new/page";
+
+import { useAppData } from "@/context/AppContext";
+
+const SideBar = () => {
+  const { searchQuery, setSearchQuery, setCategory } = useAppData();
+
+  return (
+    <Sidebar>
+      <SidebarHeader className="bg-white text-2xl font-bold mt-5">
+        The Reading Retreat
+      </SidebarHeader>
+
+      <SidebarContent className="bg-white">
+        <SidebarGroup>
+          <SidebarGroupLabel>Search</SidebarGroupLabel>
+          <Input
+            type="text"
+            placeholder="Search Your Desired Blog"
+            value={searchQuery}
+            onChange={(element) => setSearchQuery(element.target.value)}
+          />
+
+          <SidebarGroupLabel>Categories</SidebarGroupLabel>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton onClick={() => setCategory("")}>
+                <BoxSelect /> <span>All</span>
+              </SidebarMenuButton>
+
+              {blogCategories?.map((element, index) => {
+                return (
+                  <SidebarMenuButton
+                    key={index}
+                    onClick={() => setCategory(element)}
+                  >
+                    <BoxSelect /> <span>{element}</span>
+                  </SidebarMenuButton>
+                );
+              })}
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+};
+
+export default SideBar;
