649ee75ea1b3f1095dfec95f932489ef01204bec
Samuel Lopes <samuellopes12390@gmail.com>
2025-08-17 09:45:49 -0300

    [Blog_App] feat(service-blog): Roteado os Métodos de Comentários do Controller em Router

--- Blog_App/services/blog/src/routes/blog.ts
+++ Blog_App/services/blog/src/routes/blog.ts
@@ -1,10 +1,22 @@
 import express from "express";
 
-import { getAllBlogs, getSingleBlog } from "../controllers/blog.js";
+import {
+  addComment,
+  deleteComment,
+  getAllBlogs,
+  getAllComments,
+  getSingleBlog,
+} from "../controllers/blog.js";
+
+import { isAuth } from "../middlewares/isAuth.js";
 
 const router = express.Router();
 
 router.get("/blog/all", getAllBlogs);
 router.get("/blog/:id", getSingleBlog);
+
+router.post("/comment/:id", isAuth, addComment);
+router.get("/comment/:id", getAllComments);
+router.delete("/comment/:id", isAuth, deleteComment);
 
 export default router;
