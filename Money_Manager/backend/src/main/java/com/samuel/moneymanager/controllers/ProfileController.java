5f6d8308b639ea464dfd1c3923262e14fb6566e7
Samuel Lopes <samuellopes12390@gmail.com>
2025-08-29 17:55:44 -0300

    [Money_Manager] feat(backend-controller): Estruturado o Endpoint que Realiza o Login do Usuário

--- Money_Manager/backend/src/main/java/com/samuel/moneymanager/controllers/ProfileController.java
+++ Money_Manager/backend/src/main/java/com/samuel/moneymanager/controllers/ProfileController.java
@@ -1,5 +1,7 @@
 package com.samuel.moneymanager.controllers;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.samuel.moneymanager.dtos.AuthDTO;
 import com.samuel.moneymanager.dtos.ProfileDTO;
 import com.samuel.moneymanager.services.ProfileService;
 
@@ -36,4 +39,21 @@ public class ProfileController {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Activation Token not Found or Already Used");
 		}
 	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<Map<String, Object>> login(@RequestBody AuthDTO authDTO) {
+		try {
+			if(!profileService.isAccountActive(authDTO.getEmail())) {
+				return ResponseEntity.status(HttpStatus.FORBIDDEN).body(Map.of("message", 
+						"Account is not Active. Please Activate Your Account First."));
+			}
+			
+			Map<String, Object> response = profileService.authenticateAndGenerateToken(authDTO);
+			
+			return ResponseEntity.ok(response);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("message", e.getMessage()));
+		}
+	}
+	
 }
